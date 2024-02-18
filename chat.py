ff2ed76edc459693943ffac6c55e5c8b005e83d6
MaggellanN <ivanstelmah81@ya.ru>
2024-02-18 22:35:39 +0700

    Basic version

--- chat.py
+++ chat.py
@@ -1,22 +1,91 @@
+import json
+import uuid
+
 
 class Chat:
+    def list(self):
+        # Function for listing all the chats
+        try:
+            with open("chat.json", 'r') as f:
+                chats = json.load(f)
+            return chats
+        except FileNotFoundError:
+            print("No chats found. Creating a new one...")
+            return self.create()
 
     def create(self):
         # Function for creating a new chat
-        pass
+        name = input("Enter a name for your chat: ")
 
-    def load(self):
-        # Function for loading a chat
-        pass
+        chat = {
+            "name": name,
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."}
+            ]
+        }
 
-    def save(self):
-        # Function for saving a chat
-        pass
+        return self.save(chat)
 
-    def delete(self):
+    def save(self, chat, chat_id=None):
+        # Функция для сохранения чата
+        try:
+            with open("chat.json", 'r+') as f:
+                chats = json.load(f)
+        except FileNotFoundError:
+            chats = {"chats": {}}
+
+        if chat_id:
+            chats["chats"][chat_id] = chat  # update existing chat
+        else:
+            new_id = self.generate_new_id()
+            chats["chats"][new_id] = chat  # create new chat
+
+        with open("chat.json", 'w') as f:
+            json.dump(chats, f)
+
+        return chats
+
+    def generate_new_id(self):
+        # Function for generating a new ID
+        return str(uuid.uuid4())
+
+    def delete(self, chat_id):
         # Function for deleting a chat
-        pass
+        chats = self.list()
+        if chat_id in chats["chats"]:
+            print(f"Deleting chat '{chats['chats'][chat_id]['name']}'")
+            while True:
+                confirmation = input("Are you sure you want to delete this chat? (y/n): ").lower()
+                if confirmation == "y":
+                    del chats["chats"][chat_id]
+                    with open("chat.json", 'w') as f:
+                        json.dump(chats, f)
+                    print("Chat deleted.")
+                    break
+                elif confirmation == "n":
+                    print("Chat not deleted.")
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'.")
+        else:
+            print("Chat not found.")
 
-    def list(self):
-        # Function for listing all chats
-        pass
+    def get_id(self, command):
+        # Function for getting the ID of a chat
+        chats = self.list()
+        chats_count = len(chats["chats"])
+        if 1 <= int(command) <= chats_count:
+            chat_id: str = list(chats["chats"].keys())[int(command) - 1]
+            return chat_id
+        else:
+            print("Invalid chat number.")
+            return None  # Return None if chat number is invalid
+
+    def load(self, chat_id):
+        # Function for loading a chat
+        chats = self.list()
+        if chat_id in chats["chats"]:
+            return chats["chats"][chat_id]
+        else:
+            print("Chat not found.")
+            return None
--- main.py
+++ main.py
@@ -1,27 +1,117 @@
 from configurations import Config
+from chat import Chat
+from openai import OpenAI
 
 
 def main():
     config_manager = Config()
-    print(f"Hello!\nCurrent Configuration: {config_manager.get()}")
-    print("To start a chat enter /chat, to change the configuration enter /config, and to exit enter /exit.")
+    print("To start a chat, enter the command '/chat'. To change the configuration, enter the command '/config'. "
+          "If you need to view the current configuration, enter the '/get' command. "
+          "To exit the chat, enter the command '/exit'.")
+
+    commands = {
+        "/config": config_manager.edit,
+        "/chat": chat_menu,
+        "/exit": exit,
+        "/get": config_manager.get
+    }
 
     while True:
-        command = input(">>> ").lower()
-        print(command)
+        command = input("Main menu >>> ").lower()
 
-        if command in ["/config", "/cnfg", "/cnf"]:
-            config_manager.edit()
-
-        elif command in ["/exit", "/quit", "/e", "/q", "/z^"]:
-            break
-
-        elif command == "/chat":
-            pass
-
+        if command in commands:
+            commands[command]()
         else:
             print("Invalid command. Please try again.")
 
 
+def chat_menu():
+    # Function for handling the chat menu
+    chat_manager = Chat()
+    chats = chat_manager.list()
+
+    commands = {
+        "/new": chat_manager.create,
+        "/menu": main,
+        "/chats": lambda: chat_menu()
+    }
+
+    for i, chat in enumerate(chats["chats"].values(), 1):
+        print(f"{i}. \"{chat['name']}\" tagged as {i}.")
+
+    print("Enter the chat number or enter '/new' to create a new chat, "
+          "if you need to return to the menu, enter '/menu'. To show the list of chats, enter '/chats': ")
+    while True:
+        command = input("Chat menu >>> ")
+        if command in commands:
+            commands[command]()
+        elif command.isdigit():
+            chat_id = chat_manager.get_id(command)
+            handle_chat(chat_id)
+        else:
+            print("Invalid command. Please try again.")
+
+
+def handle_chat(chat_id):
+    # Function for handling a chat
+    chat_manager = Chat()
+    chat_command = {
+        "/delete": lambda: (chat_manager.delete(chat_id), main()),
+        "/menu": main,
+        "/back": chat_menu,
+        "/load": lambda: (dialogue(chat_manager.load(chat_id), chat_id), main())
+    }
+
+    chat_name = chat_manager.list()["chats"][chat_id]["name"]
+
+    print("Type '/delete' to delete this chat, or '/menu' to return to the main menu or "
+          "'/back' to return to the chat menu. Type '/load' to load this chat.")
+
+    while True:
+        command = input(f"{chat_name} menu >>> ")
+        if command in chat_command:
+            chat_command[command]()
+        else:
+            print("Invalid command. Please try again.")
+
+
+def dialogue(chat, chat_id):
+    chat_manager = Chat()
+    config_manager = Config()
+
+    client = OpenAI(api_key=config_manager.get()["api_key"])
+    print(f"---{chat['name']}---")
+    for message in chat["messages"]:
+        if message["role"] == "user":
+            print(f"You: {message['content']}")
+            continue
+        if message["role"] == "system":
+            continue
+        print(f"{message['role']}: {message['content']}")
+
+    while True:
+        message = input("You: ")
+        if message == "/exit":
+            print("Chat exited and saved.")
+            chat_manager.save(chat, chat_id)
+            chat_menu()
+        else:
+            chat["messages"].append({"role": "user", "content": message})
+            response = client.chat.completions.create(
+                model=config_manager.get()["model"],
+                temperature=config_manager.get()["temperature"],
+                messages=chat["messages"]
+            )
+            if response.choices:
+                for choice in response.choices:
+                    if choice.finish_reason == 'stop' and choice.message:
+                        print(f"Assistant: {choice.message.content}")
+                        chat["messages"].append({"role": "assistant", "content": choice.message.content})
+            else:
+                print("No response from the assistant.")
+
+
 if __name__ == '__main__':
+    config_manager = Config()
+    print(f"Hello!\nCurrent Configuration: {config_manager.get()}")
     main()
